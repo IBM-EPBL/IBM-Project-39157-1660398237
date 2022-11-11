8fdb7bca7a26da66d84cc0a9447472e49fcd9ee6
Bhairavi6501 <104494048+Bhairavi6501@users.noreply.github.com>
2022-11-11 16:23:49 +0530

    Update Add The Convolution Layer.py

--- Final Deliverables/Model Building/Add The Convolution Layer.py
+++ Final Deliverables/Model Building/Add The Convolution Layer.py
@@ -1,3 +1,7 @@
+#The convolution Layer is defined as :
+#cnn.add(tf.keras.layers.Conv2D(filters=64,kernel_size=3,activation ="relu",input_shape =[64,64,1]))
+
+
 cnn.add(tf.keras.layers.Conv2D(filters=64,kernel_size=3,activation ="relu",input_shape =[64,64,1]))
 
 cnn.add(tf.keras.layers.MaxPool2D(pool_size = 2,strides=2))
