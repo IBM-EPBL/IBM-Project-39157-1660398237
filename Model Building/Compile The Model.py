a8b516547e47275faa59624f03a1c3f9a23477c4
Bhairavi6501 <104494048+Bhairavi6501@users.noreply.github.com>
2022-11-20 10:27:00 +0530

    Create Compile The Model.py

---
+++ Model Building/Compile The Model.py
@@ -0,0 +1,8 @@
+# Compile the model
+
+  #Optimizer - adam
+  #loss function - categorical_crossentropy
+  #Metrics used for validating the model - accuracy 
+
+
+cnn.compile(optimizer="adam",loss="categorical_crossentropy" ,metrics =["accuracy"])
